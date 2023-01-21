f7ee92e00403712a113f8e14d22077ffb5d57401
Kuiliang Zhang <kzhang@student.ubc.ca>
2023-01-21 11:31:24 -0800

    fix submitbtn

--- src/components/submitBtn/index.jsx
+++ src/components/submitBtn/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Form } from '@alifd/next';
 import styles from './index.module.scss';
 
+// comment
 const SubmitBtn = (props) => {
   return (
     <Form.Submit {...props} className={`${styles.btn} ${props.className}`}>
